786e68ab971f7c42ca63fd85c7fae4b284a892cd
jcorreaITM <42014919+jcorreaITM@users.noreply.github.com>
2018-10-03 17:57:16 -0500

    Parcial2

---
+++ Parcial2_20182.cpp
@@ -0,0 +1,32 @@
+/* Parcial #2 - Lenguajes de Programacion LPW-72 Grupo 1 2018_2 *
+*  Docente: Juan Camilo Correa Chica                            *
+*  Departamento de Electronica y Telecomunicaciones              *
+*  Instituto Tecnologico Metropolitano - ITM                    *
+*                                                               *
+*  Nombres: 1)                                                  *
+*           2)                                                  *
+*  Documentos: 1)                                               *
+*              2)                                               *
+*  Hora aproximada del commit:                                  *
+*****************************************************************/
+
+/*NOTA: Comente apropiadamente cada linea de codigo de sus funciones. El comentario debe explicar que hace la linea de codigo comentada */
+
+/*Diseñe y codifique una funcion que permita identificar si dos cadenas de caracteres
+son anagramas. Un anagrama es una palabra que se conforma utilizando los caracteres (letras)
+de otra palabra, es decir es una copia de una palabra pero con las letras en distinto orden.
+Su funcion debe recibir como parametros de entrada las dos cadenas de caracteres y debe retornar
+un valor booleano que indica si las palabras son anagramas o no. */
+
+/*Diseñe y codifique una funcion que calcule el producto cruz y el producto punto entre dos
+vectores tridimensionales 
+Ayuda teorica: https://www.geoan.com/analitica/vectores/producto_cruz.html https://www.geoan.com/analitica/vectores/producto_punto.html*/
+
+/* Diseñe y codifique una funcion que permita convertir un numero complejo a su representacion en
+fasores o que convierta el fasor a su representación en coordenadas rectangulares. Si se le ingresa
+un complejo rectangular, entonces al convertirlo en fasor se debe imprimir "Magnitud: __, Angulo: ___"
+el angulo es en radianes. Si se ingresa un fasor, entonces al convertirlo en complejo rectangular se
+debe imprimir "Real: __, Imaginaria: __" 
+Ayuda teorica: http://tutorialesdeelectronicabasica.blogspot.com/2016/06/numeros-complejos-y-fasores-en-polar-y.html*/
+
+/*NOTA: Comente apropiadamente cada linea de codigo de sus funciones. El comentario debe explicar que hace la linea de codigo comentada */
